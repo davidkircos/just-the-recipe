33aebfd3ebae63af09223b7a0c9bc76860d45fba
David Kircos <davidkircos@gmail.com>
2023-11-26 18:36:52 -0700

    update

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,5 +1,3 @@
-import { CMS_NAME } from "../lib/constants";
-
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
@@ -7,9 +5,9 @@ const Intro = () => {
         Just The Recipe
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        No articles or stories, just recipes by{" "}
+        No articles or stories, <br className="lg:hidden"></br> just recipes by{" "}
         <a
-          href="https://davidkircos.com"
+          href="https://www.davidkircos.com"
           target="_blank"
           className="underline hover:text-blue-600 duration-200 transition-colors"
         >
--- components/meta.tsx
+++ components/meta.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+import Head from "next/head";
+import { HOME_OG_IMAGE_URL } from "../lib/constants";
 
 const Meta = () => {
   return (
@@ -34,11 +34,14 @@ const Meta = () => {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+        content={`Recipe website, with just the recipe.`}
       />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <meta
+        property="og:image"
+        content={`${process.env.NEXT_PUBLIC_BASE_URL}/og.png`}
+      />
     </Head>
-  )
-}
+  );
+};
 
-export default Meta
+export default Meta;
